03a39b9496a24d4989733591c3db564a93b9bdc7
mumu2plus <mumu2plus@gmail.com>
2018-09-17 15:05:11 +0800

    just a try 2

--- .idea/src/com/mumu2plus/hello/hello.java
+++ .idea/src/com/mumu2plus/hello/hello.java
@@ -2,6 +2,6 @@ package com.mumu2plus.hello;
 
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello Github!");
+        System.out.println("Hello Github! I'm mumu2plus!");
     }
 }
